bc7344c72430fd2e09f0040ad21a4945aa3e7c61
GianlucaFiorani <gianluca.fiorani97@gmail.com>
2025-05-23 20:35:15 +0200

    added css

--- src/components/MeteoDetails.jsx
+++ src/components/MeteoDetails.jsx
@@ -10,18 +10,20 @@ const MeteoDetails = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [date, setDate] = useState([]);
 
   const fetchMeteoNow = async () => {
     console.log("fetching...");
     setIsLoading(true);
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${params.lat}&lon=${params.lon}&appid=2b97b0e9fac8ab88425abf5337b025fe&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${params.lat}&lon=${params.lon}&appid=2b97b0e9fac8ab88425abf5337b025fe&units=metric&lang=it`
       );
       if (response.ok) {
         const meteoData = await response.json();
         setMeteoWeather(meteoData.weather[0]);
         setMeteoNow(meteoData.main);
+        setDate(new Date(meteoData.dt * 1000).toDateString());
         console.log(meteoWeather);
         console.log(meteoNow);
       } else {
@@ -40,7 +42,7 @@ const MeteoDetails = () => {
     setIsLoading(true);
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${params.lat}&lon=${params.lon}&appid=2b97b0e9fac8ab88425abf5337b025fe&units=metric`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${params.lat}&lon=${params.lon}&appid=2b97b0e9fac8ab88425abf5337b025fe&units=metric&lang=it`
       );
       if (response.ok) {
         const meteoData = await response.json();
@@ -56,6 +58,30 @@ const MeteoDetails = () => {
       setIsLoading(false);
     }
   };
+  const weekDay = (d) => {
+    switch (new Date(d.dt * 1000).getDay()) {
+      case 0:
+        return "Dom";
+
+      case 1:
+        return "Lun";
+
+      case 2:
+        return "Mar";
+
+      case 3:
+        return "Mer";
+
+      case 4:
+        return "Gio";
+
+      case 5:
+        return "Ven";
+
+      case 6:
+        return "Sab";
+    }
+  };
   useEffect(() => {
     fetchMeteoNow();
     fetchMeteoWeek();
@@ -63,10 +89,15 @@ const MeteoDetails = () => {
   }, []);
   return (
     <div
+      className="pt-5"
       style={{
         height: "100%",
         backgroundImage: `url("  ${
-          meteoWeather.main === "Rain" ? "https://i.gifer.com/Xm74.gif" : meteoWeather.main === "Clouds" ? "https://i.gifer.com/9vZI.gif" : ""
+          meteoWeather.main === "Rain" || meteoWeather.main === "Drizzle"
+            ? "https://i.gifer.com/Xm74.gif"
+            : meteoWeather.main === "Clouds"
+            ? "https://i.gifer.com/9vZI.gif"
+            : ""
         } ")`,
         backgroundPosition: "center",
         backgroundSize: "cover",
@@ -74,18 +105,21 @@ const MeteoDetails = () => {
       }}
     >
       <Container>
-        <div className="meteo">
-          <div className="text-center pt-5 m-0">
-            <h2>{params.name}</h2>
-          </div>
-          <div className="text-center">
-            <img src={`https://openweathermap.org/img/wn/${meteoWeather.icon}@2x.png`} alt="" width="200px" />
-          </div>
-          <div className="text-center">
-            <p>{meteoWeather.description}</p>
-          </div>
-          <div className="text-center ">
-            <h1>{Math.round(parseFloat(meteoNow.temp))}°</h1>
+        <div className="d-flex justify-content-center">
+          <div className="meteo">
+            <div className="text-center mt-2 ">
+              <h2>{params.name}</h2>
+            </div>
+            <div className="text-center ">{date}</div>
+            <div className="text-center">
+              <img src={`https://openweathermap.org/img/wn/${meteoWeather.icon}@4x.png`} alt="" width="200px" />
+            </div>
+            <div className="text-center">
+              <p>{meteoWeather.description}</p>
+            </div>
+            <div className="text-center ">
+              <h1>{Math.round(parseFloat(meteoNow.temp))}°</h1>
+            </div>
           </div>
         </div>
         <Row className="flex-nowrap mt-4">
@@ -95,7 +129,7 @@ const MeteoDetails = () => {
               <div className="text-center">
                 <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} alt="" />
               </div>
-              <div className="text-center">{day.dt_txt}</div>
+              <div className="text-center">{weekDay(day)}</div>
             </Col>
           ))}
         </Row>
